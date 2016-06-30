4cb8921f07230a4ba03a394e3078c0d5df061a45
YourtionGuo <yourtion@gmail.com>
2016-06-30 10:18:12 +0800

    add Android Project

---
+++ Android/app/src/main/java/com/yourtion/superid_rn/SuperID.java
@@ -0,0 +1,13 @@
+package com.yourtion.superid_rn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuperID extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_id);
+    }
+}
